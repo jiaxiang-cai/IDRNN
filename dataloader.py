aa2c07b144868f57aaa3c3897a3af634d92801d2
jiaxiang-cai <jiaxiang.cai@weizmann.ac.il>
2023-07-13 01:44:29 +0300

    基本构建

---
+++ dataloader.py
@@ -0,0 +1,35 @@
+import torch
+from torch.utils.data import Dataset
+import torch.nn.functional as func
+import torchvision.models as models
+import torchvision.transforms as tfms
+from PIL import Image
+from skimage.draw import random_shapes
+
+# 不知道哪里找到的野鸡模板，后面改
+
+class CustomDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.images = []
+        self.labels = []
+        self.load_data()
+
+    def load_data(self):
+        for i in range(1, 101):
+            img = Image.open(self.root_dir + str(i) + '.png')
+            self.images.append(img)
+            self.labels.append(i)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        img = self.images[idx]
+        label = self.labels[idx]
+        if self.transform:
+            img = self.transform(img)
+        return img, label
---
+++ model_GNN.py
@@ -0,0 +1,15 @@
+# GNN template
+
+# AI写的傻逼屎山代码
+import torch
+
+# Define model
+class GNN(torch.nn.Module):
+    def __init__(self):
+        super(GNN, self).__init__()
+        # Add parameters here
+        pass
+    def forward(self, x):
+        # Add forward pass here
+        pass
+
