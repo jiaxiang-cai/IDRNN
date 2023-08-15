47ea6cbc09ec8c9d0e4b9690d493c242972ad6b9
jiaxiang-cai <jiaxaing.cai@weizmann.ac.il>
2023-08-15 15:02:45 +0300

    Implement by 1D CNN through chatGPT

---
+++ dataloaderGPT.py
@@ -0,0 +1,53 @@
+import torch
+import numpy as np
+import pandas as pd
+from torch.utils.data import Dataset, DataLoader
+
+# Dictionary to map amino acids to integers
+amino_acid_to_int = {'A': 0, 'C': 1, 'D': 2, 'E': 3, 'F': 4, 'G': 5, 'H': 6, 'I': 7, 'K': 8,
+                     'L': 9, 'M': 10, 'N': 11, 'P': 12, 'Q': 13, 'R': 14, 'S': 15, 'T': 16,
+                     'V': 17, 'W': 18, 'Y': 19}
+
+# Copy from ChatGPT
+class AminoAcidDataset(Dataset):
+    def __init__(self, csv_file, max_sequence_length, num_amino_acids):
+        self.data = pd.read_csv(csv_file)
+        self.max_sequence_length = max_sequence_length
+        self.num_amino_acids = num_amino_acids
+        
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        sequence_str = self.data.iloc[idx, 0]  # Assuming the sequence is in the first column
+        
+        # Convert amino acid sequence string to a list of integers (e.g., using a dictionary)
+        sequence = [amino_acid_to_int[aa] for aa in sequence_str]
+        
+        # Perform one-hot encoding and post-padding
+        padded_sequence = np.zeros((self.max_sequence_length, self.num_amino_acids), dtype=np.float32)
+        for i, amino_acid in enumerate(sequence):
+            padded_sequence[i][amino_acid] = 1.0
+        
+        return torch.tensor(padded_sequence, dtype=torch.float32).transpose(0, 1)
+
+
+
+# Path to the CSV file containing amino acid sequences
+# csv_file_path = 'path/to/your/csv/file.csv'
+
+# Set the maximum sequence length and number of amino acids
+# max_sequence_length = 10
+# num_amino_acids = 20
+
+# Create an instance of the AminoAcidDataset
+# dataset = AminoAcidDataset(csv_file_path, max_sequence_length, num_amino_acids)
+
+# # Create a DataLoader
+# batch_size = 2
+# dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+# # Iterate through the DataLoader
+# for batch in dataloader:
+#     print("Batch shape:", batch.shape)
+#     print("Batch data:", batch)
--- model_VAE.py
+++ model_VAE.py
@@ -76,3 +76,95 @@ class DecoderLn(nn.Module):
 #         output = self.decoder(dec_input)
 #         return output
 
+# Copy from ChatGPT
+
+class EncoderGPT(nn.Module):
+    def __init__(self, input_size, latent_dim):
+        super(EncoderGPT, self).__init__()
+        
+        # Convolutional layers for local feature extraction
+        self.conv1 = nn.Conv1d(input_size, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv1d(32, 64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv1d(64, 128, kernel_size=3, padding=1)
+        
+        # Fully connected layers for mapping to latent space
+        self.fc_mean = nn.Linear(128 * 1000, latent_dim)
+        self.fc_logvar = nn.Linear(128 * 1000, latent_dim)
+        
+    def forward(self, x):
+        # Input shape: (batch_size, input_size, sequence_length)
+        
+        # Apply convolutional layers
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        
+        # Flatten the features
+        x = x.view(x.size(0), -1)
+        
+        # Calculate mean and log variance for latent distribution
+        mean = self.fc_mean(x)
+        logvar = self.fc_logvar(x)
+        
+        return mean, logvar
+
+# # Example usage
+# input_size = 20  # Number of amino acids
+# latent_dim = 3
+# sequence_length = 1000  # Padded sequence length
+
+# # Create an instance of the Encoder
+# encoder = Encoder(input_size, latent_dim)
+
+# # Generate a random input batch
+# batch_size = 32
+# input_data = torch.randn(batch_size, input_size, sequence_length)
+
+# # Forward pass through the encoder
+# mean, logvar = encoder(input_data)
+
+# print("Mean shape:", mean.shape)
+# print("Logvar shape:", logvar.shape)
+
+class DecoderGPT(nn.Module):
+    def __init__(self, latent_dim, output_size):
+        super(DecoderGPT, self).__init__()
+        
+        # Fully connected layer for mapping from latent space to hidden size
+        self.fc = nn.Linear(latent_dim, 128 * 1000)
+        
+        # Deconvolutional (transpose convolution) layers for upsampling
+        self.deconv1 = nn.ConvTranspose1d(128, 64, kernel_size=3, padding=1)
+        self.deconv2 = nn.ConvTranspose1d(64, 32, kernel_size=3, padding=1)
+        self.deconv3 = nn.ConvTranspose1d(32, output_size, kernel_size=3, padding=1)
+        
+    def forward(self, z):
+        # Input shape: (batch_size, latent_dim)
+        
+        # Fully connected layer
+        x = self.fc(z)
+        x = x.view(x.size(0), 128, 1000)
+        
+        # Apply deconvolutional layers
+        x = self.deconv1(x)
+        x = self.deconv2(x)
+        x = torch.sigmoid(self.deconv3(x))
+        
+        return x
+    
+class VAEGPT(nn.Module):
+    def __init__(self, input_size, latent_dim, output_size):
+        super(VAEGPT, self).__init__()
+        self.encoder = EncoderGPT(input_size, latent_dim)
+        self.decoder = DecoderGPT(latent_dim, output_size)
+        
+    def forward(self, x):
+        mean, logvar = self.encoder(x)
+        z = self.sample_latent(mean, logvar)
+        recon_x = self.decoder(z)
+        return recon_x, mean, logvar
+    
+    def sample_latent(self, mean, logvar):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return mean + eps * std
